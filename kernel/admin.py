e5eb1e8cedb416825eaf529ce828197f677d93ee
l_hosnarokh <l.hosnarokh@yaraholding.com>
2019-10-21 14:58:24 +0330

    add Expense model and admin

--- kernel/admin.py
+++ kernel/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Expense, Income
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'amount', 'description', 'created_date',)
+    fields = ('amount', 'description', 'created_date', 'updated_date')
+
+
+@admin.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'amount', 'description', 'created_date',)
+    fields = ('amount', 'description', 'created_date', 'updated_date')
--- kernel/models.py
+++ kernel/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Expense(models.Model):
+    created_date = models.DateTimeField(_('created date'), auto_now_add=True)
+    updated_date = models.DateTimeField(_('updated date'), auto_now=True)
+    amount = models.BigIntegerField(_('amount'), )
+    description = models.TextField(_('description'), )
+    user = models.ForeignKey(User, _('owner'), related_name='expenses')
+
+    class Meta:
+        verbose_name = _('Expense')
+        verbose_name_plural = _('Expenses')
+
+    def __str__(self):
+        return '%s-%s' % (self.amount, self.description)
+
+
+class Income(models.Model):
+    created_date = models.DateTimeField(_('created date'), auto_now_add=True)
+    updated_date = models.DateTimeField(_('updated date'), auto_now=True)
+    amount = models.BigIntegerField(_('amount'), )
+    description = models.TextField(_('description'), )
+    user = models.ForeignKey(User, _('owner'), related_name='incomes')
+
+    class Meta:
+        verbose_name = _('Income')
+        verbose_name_plural = _('Incomes')
+
+    def __str__(self):
+        return '%s-%s' % (self.amount, self.description)
